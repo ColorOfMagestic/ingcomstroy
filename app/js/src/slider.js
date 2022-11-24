2e1cbae876e033f5dcf2f1276260a9a3da9c6db2
Kuznetsov Pavel <colorOfMagic@mail.ru>
2022-11-24 21:29:10 +0500

    Initialization slider for  "partners"

--- app/js/src/slider.js
+++ app/js/src/slider.js
@@ -1,19 +1,23 @@
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: `[data-role="next"]`,
-//         prevEl: `[data-role="prev"]`,
-//     },
-//     breakpoints: {
-//       320: {
-//       },
-//       768: {
-//       },
-//       1170: {
-//       }
-//     }
-// });
+const swiper = new Swiper('.partners_swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: `[data-role="next"]`,
+        prevEl: `[data-role="prev"]`,
+    },
+    breakpoints: {
+      320: {
+      },
+      768: {
+      },
+      1170: {
+      }
+    },
+    autoplay: {
+        delay: 3000,
+    }
+});

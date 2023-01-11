dab090175d5d11048fa71bf47eb449b78b36905b
Kuznetsov Pavel <colorOfMagic@mail.ru>
2023-01-11 18:14:33 +0500

    Edits in "Trend" block

--- app/js/src/slider.js
+++ app/js/src/slider.js
@@ -43,3 +43,67 @@ const feedback = new Swiper('.feedback_swiper ', {
       }
     },
 });
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 4,
+  slidesPerView: 9,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: `[data-role="mySwiper2_next"]`,
+    prevEl: `[data-role="mySwiper2_prev"]`,
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  autoplay: {
+    delay: 3000,
+}
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  spaceBetween: 4,
+  slidesPerView: 9,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper4", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: `[data-role="mySwiper4_next"]`,
+    prevEl: `[data-role="mySwiper4_prev"]`,
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  autoplay: {
+    delay: 3000,
+}
+});
+
+var swiper = new Swiper(".mySwiper5", {
+  spaceBetween: 4,
+  slidesPerView: 9,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper6", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: `[data-role="mySwiper6_next"]`,
+    prevEl: `[data-role="mySwiper6_prev"]`,
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  autoplay: {
+    delay: 3000,
+}
+});
